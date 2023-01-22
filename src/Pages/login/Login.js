bc8d2e9d1833e5d656f8a4bd10be7b87b0ab5e30
Muhammad Awais <awaisshaikh94@gmail.com>
2023-01-22 20:36:58 +0500

    login finalized

--- src/Pages/login/Login.js
+++ src/Pages/login/Login.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../login/login.css";
-import image from "../../assets/img/phone-xxl.png";
+import image from "../../assets/img/logo.png";
 
 function Login() {
   return (
     <>
       <div>
-        <div className="screen1">
+        <div className="screen1 gradient-body">
           <div className="container">
             <div className="row">
               <div className="col-7"></div>
+
               <div className="col-5">
                 <div className="screen1-inner">
                   <div className="card">
                     <div className="card-body">
                       <img src={image} />
-                      <h5 className="card-title">
-                        <span>META</span> TALENT{" "}
-                      </h5>
 
                       <div className="form-inner">
                         <input
@@ -35,25 +33,26 @@ function Login() {
                           placeholder=" Password"
                           aria-describedby="emailHelp"
                         />
-                        <div className="screen-btn">
-                          <a className="btn btn-primary w-100" type="button">Button</a> 
+                        <div className="yellow-btn">
+                          <a className="w-100 text-center mt-4" type="button">SIGN IN</a> 
                         </div>
 
-                        <span><a href="#">Forget Password?</a></span>
+                        <div className="forget-pass mt-2">
+                          <a className="text-black-50">Forget password?</a>
+                        </div>
                         <div className="wrapper-btn">
                           <p>or continue with</p>
                         </div>
 
-                        <div className="Sign-btn">
-                          <a className="btn btn-outline-primary  w-50" type="button">Sign in with Google</a>
+                        <div className="sign-btn-google">
+                          <button type="button" class="login-with-google-btn" >
+                            Sign in with Google
+                          </button>
                         </div>
 
-
-                        <div className="register-btn">
-                          <a className="btn btn-primary w-50" type="button">REGISTER NOW</a> 
+                        <div className="purple-btn mt-5 mb-3 text-center">
+                          <a>REGISTER NOW</a> 
                         </div>
-
-                      
                       </div>
                     </div>
                   </div>
